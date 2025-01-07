0b73884440f5a924356a556505dafa83dba05bea
faqihfnf <faqih.fnf@gmail.com>
2025-01-07 07:34:14 +0700

    first commit

---
+++ react-dasar/src/hello-world/HelloWorld.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function HelloWorld() {
+  return (
+    <div>
+      <HeaderHelloWorld />
+      <ParagraphHelloWorld />
+    </div>
+  );
+}
+
+function HeaderHelloWorld() {
+  return <h1>Hello World</h1>;
+}
+
+function ParagraphHelloWorld() {
+  return <p>Selamat belajar react dasar</p>;
+}
