34b783b70b2ca5d521b59ca68889b43ee51e306b
faqihfnf <faqih.fnf@gmail.com>
2025-01-11 10:54:31 +0700

    props update

--- react-dasar/src/hello-world/HelloWorld.jsx
+++ react-dasar/src/hello-world/HelloWorld.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 export default function HelloWorld() {
+  const props = {
+    text: "Hello World dari spread props",
+  };
   return (
     <div>
       <HeaderHelloWorld props={{ text: "Hello World" }} />
       <TextHelloworld />
       <ParagraphHelloWorld />
+      <SpreadHelloWorld {...props} />
     </div>
   );
 }
@@ -23,5 +27,9 @@ function TextHelloworld({ text = "Default Text" }) {
 
 function ParagraphHelloWorld() {
   const text = "Selamat belajar react dasar";
+  return <p>{text.toLowerCase()}</p>;
+}
+
+function SpreadHelloWorld({ text = "Default Text" }) {
   return <p>{text.toLowerCase()}</p>;
 }
