8dc34b2f1c6d6ba79163e9a42be51531d4d2fa07
faqihfnf <faqih.fnf@gmail.com>
2025-01-18 19:20:45 +0700

    maintain state

---
+++ react-dasar/src/counter/CounterApp.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import Counter from "./Counter";
+
+export default function CounterApp() {
+  const [show, setShow] = useState(true);
+  function handleChange(e) {
+    setShow(e.target.checked);
+  }
+  return (
+    <div>
+      {/* <Counter name={"First"} />
+      {show && <Counter name={"Second"} />} */}
+      {show ? <Counter name={"First"} /> : <Counter name={"Second"} />}
+      <input type="checkbox" checked={show} onChange={handleChange} id="show" /> Show Counter
+    </div>
+  );
+}
