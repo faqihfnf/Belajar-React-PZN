42107757e87d06062c25768ae8d1c91c6ff910ff
faqihfnf <faqih.fnf@gmail.com>
2025-01-19 23:18:32 +0700

    useEffect with dependencies

--- react-dasar/src/product/ProductList.jsx
+++ react-dasar/src/product/ProductList.jsx
@@ -3,25 +3,34 @@ import Product from "./Product";
 
 export default function ProductList() {
   const [products, setProducts] = useState([]);
-  const loaded = useRef(false);
+  //   const loaded = useRef(false);
+  const [loaded, setLoaded] = useState(false);
+
+  function handleClick() {
+    console.log("clicked");
+    setLoaded(true);
+  }
+
+  useEffect(() => {
+    console.log("empty array");
+  }, []);
 
   useEffect(() => {
     console.log("mounted");
-    if (loaded.current === false) {
+    if (loaded) {
       fetch("/product.json")
         .then((res) => res.json())
-        .then((data) => setProducts(data))
-        .then(() => (loaded.current = true));
+        .then((data) => setProducts(data));
     }
-
-    return () => {
-      console.log("unmounted");
-    };
-  });
+    // return () => {
+    //   console.log("unmounted");
+    // };
+  }, [loaded]);
 
   return (
     <div>
       <h1>Product List</h1>
+      <button onClick={handleClick}>Load Product</button>
       {products.map((product) => {
         return <Product key={product.id} product={product} />;
       })}
