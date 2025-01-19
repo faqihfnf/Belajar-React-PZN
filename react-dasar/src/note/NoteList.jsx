09ee3ff152b087c5f7c743129900ed4bfd2dda2d
faqihfnf <faqih.fnf@gmail.com>
2025-01-19 23:59:16 +0700

    useMemo

--- react-dasar/src/note/NoteList.jsx
+++ react-dasar/src/note/NoteList.jsx
@@ -1,14 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useMemo, useRef, useState } from "react";
 import Note from "./Note";
 import { NoteContext } from "./NoteContext";
 
 export default function NoteList() {
   const notes = useContext(NoteContext);
+  const [search, setSearch] = useState("");
+  const searchInput = useRef(null);
+
+  const filteredNotes = useMemo(() => {
+    console.log("filteredNotes");
+    return notes.filter((note) => note.text.includes(search));
+  }, [notes, search]);
+
+  function handleSearch(e) {
+    console.log("handleSearch");
+    setSearch(searchInput.current.value);
+  }
 
   return (
     <div>
+      <input ref={searchInput} type="text" placeholder="search" />
+      <button onClick={handleSearch}>Search</button>
       <ul>
-        {notes.map((note) => (
+        {filteredNotes.map((note) => (
           <li key={note.id}>
             <Note note={note} />
           </li>
