bb46274b9f8ad192875b935c81678f7b10580a36
faqihfnf <faqih.fnf@gmail.com>
2025-01-24 00:31:26 +0700

    navigation, link element and navlink element

--- react-router/src/DataLayout.jsx
+++ react-router/src/DataLayout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Outlet } from "react-router";
+import { NavLink, Outlet } from "react-router";
+import "./data.css";
 
 export default function DataLayout() {
   return (
@@ -7,8 +8,27 @@ export default function DataLayout() {
       <div>
         <h1>Ini Header</h1>
       </div>
+      <div>
+        <ul>
+          <li>
+            <NavLink to="/data/product">Product</NavLink>
+          </li>
+          <li>
+            <NavLink to="/data/seller">Seller</NavLink>
+          </li>
+          <li>
+            <NavLink to="/data/customer">Customer</NavLink>
+          </li>
+          <li>
+            <NavLink to={{ pathname: "/data/product", search: "?id=1", hash: "#test" }}>Product</NavLink>
+          </li>
+        </ul>
+      </div>
       {/* Outlet digunakan untuk menampilkan child component / component-component yang ada sebagai nested router  */}
-      <Outlet />
+      <div>
+        {" "}
+        <Outlet />
+      </div>
       <div>
         <h1>Ini Footer</h1>
       </div>
