69f11cdb1c7705fc9c410a8ba0698ed3a7e3a4fb
faqihfnf <faqih.fnf@gmail.com>
2025-01-24 00:43:53 +0700

    useNavigate

--- react-router/src/Home.jsx
+++ react-router/src/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 export default function Home() {
+  const navigate = useNavigate();
+  function handleClick() {
+    navigate("/data");
+  }
   return (
     <div>
       <h1>Home</h1>
       <p>Ini adalah halaman Home</p>
+      <button onClick={handleClick}>Go To Data Page</button>
     </div>
   );
 }
