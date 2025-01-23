bfce94826c892405166cea332fd267cdc2551ac0
faqihfnf <faqih.fnf@gmail.com>
2025-01-23 07:22:28 +0700

    implementation routing with Routes and Route component

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -1,13 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router";
+// import "./index.css";
+// import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./Home.jsx";
+import About from "./About.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
