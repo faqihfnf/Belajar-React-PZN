4083aae899d16f9ab53207c87a064669418e1549
faqihfnf <faqih.fnf@gmail.com>
2025-01-23 23:28:54 +0700

    useParams

--- react-router/src/ProductDetail.jsx
+++ react-router/src/ProductDetail.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { useParams } from "react-router";
 
 export default function ProductDetail() {
+  const params = useParams();
+
   return (
     <div>
-      <h1>ProductDetail</h1>
+      <h1>ProductDetail {params.id}</h1>
       <p>Ini adalah halaman ProductDetail</p>
     </div>
   );
