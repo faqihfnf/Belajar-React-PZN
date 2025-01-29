ed68fa6191a9c9c94d2726b94e4223fb0c37b388
faqihfnf <faqih.fnf@gmail.com>
2025-01-29 12:10:35 +0700

    actioin parameter

--- react-redux/src/Counter.jsx
+++ react-redux/src/Counter.jsx
@@ -13,11 +13,21 @@ export default function Counter() {
   function handleDecrement() {
     dispatch(decrement());
   }
+
+  function handleIncrementDouble() {
+    dispatch(increment(2));
+  }
+
+  function handleDecrementDouble() {
+    dispatch(decrement(2));
+  }
   return (
     <div>
       <h1>Counter: {counter}</h1>
       <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleIncrementDouble}>Increment Double</button>
       <button onClick={handleDecrement}>Decrement</button>
+      <button onClick={handleDecrementDouble}>Decrement Double</button>
     </div>
   );
 }
--- react-redux/src/counterSlice.js
+++ react-redux/src/counterSlice.js
@@ -4,11 +4,19 @@ export const counterSlice = createSlice({
   name: "counter",
   initialState: 0,
   reducers: {
-    increment: (state) => {
-      return state + 1;
+    increment: (state, action) => {
+      if (action.payload) {
+        return state + action.payload;
+      } else {
+        return state + 1;
+      }
     },
-    decrement: (state) => {
-      return state - 1;
+    decrement: (state, action) => {
+      if (action.payload) {
+        return state - action.payload;
+      } else {
+        return state - 1;
+      }
     },
   },
 });
