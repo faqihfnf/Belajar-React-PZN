2d2ad53f643ac0227215a136101d77af5ba6b111
faqihfnf <faqih.fnf@gmail.com>
2025-01-29 09:36:49 +0700

    Action on redux

--- react-redux/src/Counter.jsx
+++ react-redux/src/Counter.jsx
@@ -1,11 +1,23 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./counterSlice";
 
 export default function Counter() {
   const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  function handleIncrement() {
+    dispatch(increment());
+  }
+
+  function handleDecrement() {
+    dispatch(decrement());
+  }
   return (
     <div>
       <h1>Counter: {counter}</h1>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
     </div>
   );
 }
--- react-redux/src/counterSlice.js
+++ react-redux/src/counterSlice.js
@@ -3,5 +3,16 @@ import { createSlice } from "@reduxjs/toolkit";
 export const counterSlice = createSlice({
   name: "counter",
   initialState: 0,
-  reducers: {},
+  reducers: {
+    increment: (state) => {
+      return state + 1;
+    },
+    decrement: (state) => {
+      return state - 1;
+    },
+  },
 });
+
+// increment & decrement adalah action yang  bisa digunakan untuk merubah state
+
+export const { increment, decrement } = counterSlice.actions;
